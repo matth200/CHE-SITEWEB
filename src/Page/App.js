8f03551c42f6213ff0ca48c2ab61e00ad2970a23
r0 <mattheo.delelis@gmail.com>
2025-02-04 11:53:42 +0100

    test font

--- src/Page/App.js
+++ src/Page/App.js
@@ -1,25 +1,43 @@
 import logo from '../image/CHE.png';
 import '../css/App.scss';
 
-function AppPage() {
+import React, { useState } from "react";
+import { useTrail, a } from "@react-spring/web";
+
+import styles from "../css/styles.module.scss";
+
+const Trail= ({ open, children }) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  });
   return (
-    <div className="AppPage Page">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className={styles.trailsText} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
     </div>
   );
+};
+
+function AppPage() {
+  const [open, set] = useState(true);
+return (
+  <div className={styles.container} onClick={() => set((state) => !state)}>
+    <Trail open={open}>
+      <span>Lorem</span>
+      <span>Ipsum</span>
+      <span>Dolor</span>
+      <span>Sit</span>
+    </Trail>
+  </div>
+);
 }
 
 export default AppPage;
