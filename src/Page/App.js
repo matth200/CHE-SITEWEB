67ee1ae1b7ce46feac86d2732ded86f23529db09
r0 <mattheo.delelis@gmail.com>
2025-02-04 13:14:58 +0100

    update design

--- src/Page/App.js
+++ src/Page/App.js
@@ -4,6 +4,8 @@ import "../css/App.scss";
 import React, { useState } from "react";
 import Typewriter from '../Component/TypeWriter';
 
+import GeoideImg from '../image/geoide-nous.jpg';
+
 function AppPage() {
   const [open, setOpen] = useState(false);
   const [next, setNext] = useState(false);
@@ -16,12 +18,14 @@ return (
         size={100}
         onEnd={() => setNext(true)}
       />
-      {next && <Typewriter
-        text="2025"
-        speed={100}
-        size={100}
-        onEnd={() => setOpen(true) }
-      />}
+      {next && (
+        <Typewriter
+          text="2025"
+          speed={100}
+          size={100}
+          onEnd={() => setOpen(true)}
+        />
+      )}
       <div className="delay-home" style={{ opacity: open ? 1 : 0 }}>
         <h3>Présentation de l'événément</h3>
         <p>blablabla</p>
@@ -31,8 +35,13 @@ return (
       className="AppPage2 Page"
       style={{ opacity: open ? 1 : 0, display: open ? "flex" : "none" }}
     >
-      <h1>Présentation de la meilleure équipe</h1>
-      <Typewriter text="Les CRYPTANGA" speed={100} size={60} />
+      <div className="block-left">
+        {/* <img src={GeoideImg} alt="geoide-nous"/> */}
+      </div>
+      <div className="block-right">
+        <h1>Présentation de la meilleure équipe</h1>
+        <Typewriter text="Les CRYPTANGA" speed={100} size={60} />
+      </div>
     </div>
   </>
 );
