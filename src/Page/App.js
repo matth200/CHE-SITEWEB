5115515f8c2a9d2affb4b87e33fbd7f884af79d9
Gaël Brossard <gaelbrossard@orange.fr>
2025-02-23 01:09:38 +0100

    micro update

--- src/Page/App.js
+++ src/Page/App.js
@@ -1,57 +1,56 @@
-import logo from '../image/CHE.png';
 import "../css/App.scss";
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Typewriter from '../Component/TypeWriter';
 
-import GeoideImg from '../image/geoide-nous.jpg';
 import Console from '../Component/Console';
 
 function AppPage() {
-  const [open, setOpen] = useState(false);
-  const [next, setNext] = useState(false);
-return (
-  <>
-    <div className="AppPage1 Page">
-      <Typewriter
-        text="CyberHumanum'Est"
-        speed={100}
-        size={100}
-        onEnd={() => setNext(true)}
-      />
-      {next && (
-        <Typewriter
-          text="2025"
-          speed={100}
-          size={100}
-          onEnd={() => setOpen(true)}
-        />
-      )}
-      <div className="delay-home" style={{ opacity: open ? 1 : 0 }}>
-        <h3>Présentation de l'événément</h3>
-        <p>blablabla</p>
-      </div>
-    </div>
-    <div
-      className="AppPage2 Page"
-      style={{ opacity: open ? 1 : 0, display: open ? "flex" : "none" }}
-    >
-      <div className="block-left">
-        {/* <img src={GeoideImg} alt="geoide-nous"/> */}
-      </div>
-      <div className="block-right">
-        <h1>Présentation de la meilleure équipe</h1>
-        <Typewriter text="Les CRYPTANGA" speed={100} size={60} />
-      </div>
-    </div>
-    <div
-      className="AppPage3 Page"
-      style={{ opacity: open ? 1 : 0, display: open ? "flex" : "none" }}
-    >
-      <Console name="cryptanga" machine="CHE"/>
-    </div>
-  </>
-);
+    const [open, setOpen] = useState(false);
+    const [next, setNext] = useState(false);
+    return (
+        <>
+            <div className="AppPage1 Page">
+                <Typewriter
+                    text="CyberHumanum'Est"
+                    speed={100}
+                    size={100}
+                    onEnd={() => setNext(true)}
+                />
+                {next && (
+                    <Typewriter
+                        text="2025"
+                        speed={100}
+                        size={100}
+                        onEnd={() => setOpen(true)}
+                    />
+                )}
+                <div className="delay-home" style={{opacity: open ? 1 : 0}}>
+                    <h1>Présentation de l'évenement par l'équipe du Cryptanga</h1>
+                    <h2>Le CyberHumanum'Est est un évenement répliquant une cyberguerre entre deux pays, </h2>
+                    <h2>se battant pour obtenir un contrat avec l'ile tant convoitée des Riverschelles.</h2>
+                </div>
+            </div>
+            <div
+                className="AppPage2 Page"
+                style={{opacity: open ? 1 : 0, display: open ? "flex" : "none"}}
+            >
+                <div className="block-left">
+                    {/* <img src={GeoideImg} alt="geoide-nous"/> */}
+                </div>
+                <div className="block-right">
+                    <h1>Présentation de la meilleure équipe</h1>
+                    <Typewriter text="Les CRYPTANGA" speed={100} size={60}/>
+                </div>
+            </div>
+            <div
+                className="AppPage3 Page"
+                style={{opacity: open ? 1 : 0, display: open ? "flex" : "none"}}
+            >
+                <Console name="cryptanga" machine="CHE"/>
+            </div>
+        </>
+    );
 }
 
 export default AppPage;
