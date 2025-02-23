6306bd62af5a14922f4b6b4ca5efb4d83d6acf96
matth del <mattheo.delelis@gmail.com>
2025-02-24 00:14:30 +0100

    ajout image && page de mano,tristan,bapt

---
+++ src/Page/TristanPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../css/Tristan.scss';
+
+export default function TristanPage(){
+
+    return <div className='Page TristanPage'>
+        <h1>Page de tristan</h1>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './css/index.scss';
 import AppPage from './Page/App';
 import ErrorPage from './Page/Error';
 import TermPage from './Page/Terminal';
+import ManoPage from './Page/ManoPage';
+import TristanPage from './Page/TristanPage';
+import BaptPage from './Page/BaptPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +16,9 @@ root.render(
       <Routes>
         <Route path="/" element={<AppPage />}/>
         <Route path="/term" element={<TermPage />}/>
+        <Route path="/mano" element={<ManoPage />} />
+        <Route path="/bapt" element={<BaptPage />} />
+        <Route path="/tristan" element={<TristanPage />}/>
         <Route path="*" element={<ErrorPage />}/>
       </Routes>
     </BrowserRouter>
